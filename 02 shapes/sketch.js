00116e7dd3a93cd0635c5ae1b9ef848a125a51b7
anthonypittman <47329867+anthonypittman@users.noreply.github.com>
2019-02-13 11:02:50 -0600

    stateVariable Feb 13th 2019

--- 02 shapes/sketch.js
+++ 02 shapes/sketch.js
@@ -13,8 +13,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width/2;
   y = height/2;
-  xSpeed = random(15);
-  ySpeed = random(15);
+  xSpeed = random(25);
+  ySpeed = random(25);
   background(0);
   noStroke();
 }
