3f3affe88b14496c201bdbbc063ec47f1af43050
anthonypittman <47329867+anthonypittman@users.noreply.github.com>
2019-02-28 11:02:04 -0600

    mousePressed

--- 07 Rollovers/sketch.js
+++ 07 Rollovers/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let quadrant; // 1 - topLeft, 2 - topRight, 3 - bottomLeft, 4 - bottomRight
-let fillQ1, fillQ2, fillQ3, fillQ4;
+let fillQ1 = 255, fillQ2 = 255, fillQ3 = 255, fillQ4 = 255;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -35,21 +35,29 @@ function determineQuadrant() {
 }
 
 function drawRectangle() {
+  fillQ1 += 4;
+  fillQ2 += 4;
+  fillQ3 += 4;
+  fillQ4 += 4;
   if (quadrant === 1) {
-    //
+    fillQ1 = 0;
   }
   if (quadrant === 2) {
-    //
+    fillQ2 = 0;
   }
   if (quadrant === 3) {
-    //
+    fillQ3 = 0;
   }
   if (quadrant === 4) {
-    //
+    fillQ4 = 0;
   }
+  fill(fillQ1);
   rect(0, 0, width/2, height/2);
+  fill(fillQ2);
   rect(width/2, 0, width/2 - 1, height/2);
+  fill(fillQ3);
   rect(0, height/2, width/2, height/2 - 1);
+  fill(fillQ4);
   rect(width/2, height/2, width/2 - 1, height/2 - 1);
 }
 
