f90e7f6638fcc35a9ec3e315ab56d4619676c2ec
anthonypittman <47329867+anthonypittman@users.noreply.github.com>
2019-06-05 11:03:10 -0600

    background

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let link;
+let bg;
 
 function preload() {
 
@@ -22,16 +23,23 @@ function preload() {
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  bg = loadImage("assets/BG.png");
   link = createSprite(400, 400);
-  link.addAnimation("idle", "assets/idlee01.png", "assets/idlee02.png");
+  link.addAnimation("idle", "assets/idlee01.png");
   link.addAnimation("walk", "assets/walk01.png", "assets/walk04.png");
   link.addAnimation("walkLeft", "assets/walkLeft01.png", "assets/walkLeft02.png");
   link.addAnimation("walkRight", "assets/walkRight01.png", "assets/walkRight02.png");
   link.addAnimation("walkUp", "assets/walkUp01.png", "assets/walkUp04.png");
+  link.addAnimation("death", "assets/death.png");
+  link.addAnimation("jab", "assets/jab.png");
+  link.addAnimation("jabLeft", "assets/jabLeft.png");
+  link.addAnimation("jabRight", "assets/jabRight.png");
+  link.addAnimation("jabUp", "assets/jabUp.png");
 }
 
 function draw() {
   background(220);
+  image(bg, 600, 600);
   drawSprites();
   move();
 }
@@ -41,8 +49,32 @@ function move() {
     link.changeAnimation("walk");
     link.position.y += 5;
   }
+  else if (keyDown("a")) {
+    link.changeAnimation("walkLeft");
+    link.position.x -= 5;
+  }
+  else if (keyDown("d")) {
+    link.changeAnimation("walkRight");
+    link.position.x += 5;
+  }
+  else if (keyDown("w")) {
+    link.changeAnimation("walkUp");
+    link.position.y -= 5;
+  }
+  else if(keyDown("down_arrow")) {
+    link.changeAnimation("jab");
+  }
+  else if (keyDown("left_arrow")) {
+    link.changeAnimation("jabLeft");
+  }
+  else if (keyDown("right_arrow")) {
+    link.changeAnimation("jabRight");
+  }
+  else if (keyDown("up_arrow")) {
+    link.changeAnimation("jabUp");
+  }
+
   else {
     link.changeAnimation("idle");
-    
   }
 }
